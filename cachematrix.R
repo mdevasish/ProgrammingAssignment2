cfdd590f199aaef55f2ff1870c87829ccacf1300
mdevasish <mdevasish@gmail.com>
2016-07-01 17:04:40 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix())
+                   {
+                   cinv<-NULL
+                   set<-function(insert=matrix()){
+                   x<<-insert
+                   cinv<<-NULL
+                   }
+                   get<-function()x
+                   setinv<-function(inv) cinv<<-inv
+                   getinv<-function() cinv
+                   list(set=set, get=get, setinv=setinv, getinv=getinv)
+                 }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+
+
+cacheSolve <- function(m=makeCacheMatrix(1:4,nrow=2,ncol=2), ...) 
+                {
+                calinv<-m$getinv()
+                if(!is.null(calinv))
+                  {
+                    message("getting cached data")
+                    return(calinv)
+                   }
+                data<-m$get()
+                calinv<-solve(data,...)
+                m$setinv(calinv)
+                }

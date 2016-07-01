c8be5fec1aec00ba44586d06ed3ebaf2d8e28b12
mdevasish <mdevasish@gmail.com>
2016-07-01 19:04:41 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,10 @@
 
+## Put comments here that give an overall description of what your
+## functions do
 
+## Write a short comment describing this function
+
+## makeCacheMatrix is the function with set of functions that help to scalculate the inverse function.
 makeCacheMatrix <- function(x = matrix())
                    {
                    cinv<-NULL
@@ -14,7 +19,7 @@ makeCacheMatrix <- function(x = matrix())
                  }
 
 
-
+##cacheSolve function is the function which calculates the inverse of a matrix. If the inverse is already calculated the inverse is returned back.
 
 cacheSolve <- function(m, ...) 
                 {
